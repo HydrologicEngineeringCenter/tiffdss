95ef108ec70da65e2707849fb23abb93e5dee852
Jeff Gregory <37386268+jeffsuperglide@users.noreply.github.com>
2022-08-08 15:56:05 -0500

    File cleanup, add entrypoint, and update readme (#3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 # set -x
 
-if [ "$1" == "test" ]; then
-    /app/tiffdss/tests/dss-test.sh
-fi
+KEEP_RUNNING=false
+
+# remove temp dss files if they exist
+rm -f /tmp/*.dss
+
+for opt in "$@";do
+    printf "Option: $opt\n"
+
+    if [ "$opt" == "integration" ]; then
+        /hec-dssvue-linux/hec-dssvue.sh /app/tiffdss/tests/integration/test_dss_convert_tiff.py
+        /hec-dssvue-linux/hec-dssvue.sh /app/tiffdss/tests/integration/test_dss_convert_version.py
+        /hec-dssvue-linux/hec-dssvue.sh /app/tiffdss/tests/integration/test_dss_grid_info.py
+    fi
+
+    if [ "$opt" == "unit" ]; then
+        python /app/tiffdss/tests/unit/test_libtiffdss_stats.py
+    fi
+
+    if [ "$opt" == "run" ]; then
+        KEEP_RUNNING=true
+    fi
+done
 
 # Keep container running
-while "$1" == "test"
+while :
 do
+    if [ "$KEEP_RUNNING" == "false" ]; then
+        exit
+    fi
     sleep 10000
 done
+
