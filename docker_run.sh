cf18b57866cac702fbe78baabdf57fb743398761
Jeff Gregory <37386268+jeffsuperglide@users.noreply.github.com>
2023-02-13 16:35:54 -0600

    Update workflows (#20)

--- docker_run.sh
+++ docker_run.sh
@@ -10,10 +10,5 @@ docker build -t $IMAGE .
 # run
 # - with arguments uses the entrypoint.sh
 # - without arguments assumes interactive
-if [ "$1" == "dev" ]; then
-    printf "\nRunning the container interactively with a volume\n"
-    docker run --rm -it --entrypoint /bin/bash -v $PWD/:/tiffdss/ --name $CONTAINER $IMAGE
-else
-    printf "\nRunning the container with arguments: $@\n"
-    docker run --rm --name $CONTAINER $IMAGE "$@"
-fi
+printf "\nRunning the container with arguments: $@\n"
+docker run --rm --name $CONTAINER $IMAGE "$@"
